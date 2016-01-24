970c507e36c5810f91ccaaca4e23bb5d6c534ebc
ksullivan2 <kaley.sullivan@gmail.com>
2016-01-24 16:49:26 -0500

    the dice roll!
    IT'S ALIIIIIIIIIVE

---
+++ kivy_yahtzee/Dice.py
@@ -0,0 +1,21 @@
+from kivy.uix.behaviors import ToggleButtonBehavior
+from kivy.uix.image import Image
+from random import randint
+
+class Dice(ToggleButtonBehavior, Image):
+    def __init__(self, **kwargs):
+        self.number = 6
+        super(Dice, self).__init__(**kwargs)
+        self.source = ("images/up_state/dice" + str(self.number) + ".gif")
+        
+    def on_state(self, widget, value):
+        if value == "down":
+            self.source = ("images/down_state/dice" + str(self.number) + ".gif")
+        else:
+           self.source = ("images/up_state/dice" + str(self.number) + ".gif")
+
+    def roll(self):
+        #if state != "down":
+        self.number = randint(1,6)
+        print(self.number)
+        self.source = ("images/up_state/dice" + str(self.number) + ".gif")
--- kivy_yahtzee/main.py
+++ kivy_yahtzee/main.py
@@ -1,32 +1,27 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.properties import NumericProperty, DictProperty
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.behaviors import ToggleButtonBehavior
-from kivy.uix.image import Image
+from Dice import *
 
 from kivy.graphics import *
 
 class YahtzeeApp(App):
     def build(self):
-        return Dice()
+        return YahtzeeGame()
 
 class YahtzeeGame(BoxLayout):
+    def roll_all_dice(self):
+        for dice in self.ids["dice_layer"].children:
+             dice.roll()
+
+class RollButton(Button):
     pass
-
-class Dice(ToggleButtonBehavior, Image):
-    def __init__(self, **kwargs):
-        self.number = 2
-        super(Dice, self).__init__(**kwargs)
-        self.source = ("images/up_state/dice" + str(self.number) + ".gif")
-        
-    def on_state(self, widget, value):
-        if value == "down":
-            self.source = ("images/down_state/dice" + str(self.number) + ".gif")
-        else:
-           self.source = ("images/up_state/dice" + str(self.number) + ".gif")
-
+    
+    
+    
 
 if __name__ == '__main__':
     YahtzeeApp().run()
