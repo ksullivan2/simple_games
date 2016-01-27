4442ae328d11e9effa2a226577d236632990c44f
ksullivan2 <kaley.sullivan@gmail.com>
2016-01-26 20:02:08 -0500

    added the scorecard section to layout
    no real functionality yet

--- kivy_yahtzee/main.py
+++ kivy_yahtzee/main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
 from kivy.properties import NumericProperty, DictProperty
 from kivy.uix.relativelayout import RelativeLayout
 from Dice import *
@@ -13,14 +14,21 @@ class YahtzeeApp(App):
         return YahtzeeGame()
 
 class YahtzeeGame(BoxLayout):
+    pass
+
+class DiceLayer(BoxLayout):
     def roll_all_dice(self):
-        for dice in self.ids["dice_layer"].children:
+        for dice in self.children:
              dice.roll()
 
 class RollButton(Button):
     pass
     
-    
+class ScoreOption(BoxLayout):
+    pass
+
+class ScoreCard(BoxLayout):
+    pass
     
 
 if __name__ == '__main__':
---
+++ kivy_yahtzee/yahtzee_definitions.py
@@ -0,0 +1,9 @@
+import random
+
+score_types = ["aces","twos","threes","fours","fives","sixes",
+         "three_of_a_kind","four_of_a_kind","full_house", "small_straight",
+         "large_straight", "chance", "yahtzee", "yahtzee_bonus"]
+
+
+
+
