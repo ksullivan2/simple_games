f7e2390ccedbc9556f61150a1320d3b13ceb9c5f
ksullivan2 <kaley.sullivan@gmail.com>
2016-01-26 21:18:02 -0500

    moved class definitions into diff module

--- kivy_yahtzee/main.py
+++ kivy_yahtzee/main.py
@@ -1,13 +1,7 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.togglebutton import ToggleButton
-from kivy.properties import NumericProperty, DictProperty
-from kivy.uix.relativelayout import RelativeLayout
 from Dice import *
 from yahtzee_definitions import *
-#from kivy.graphics import *
+
 
 
 class YahtzeeApp(App):
@@ -17,31 +11,7 @@ class YahtzeeApp(App):
 class YahtzeeGame(BoxLayout):
     pass
 
-class DiceLayer(BoxLayout):
-    def roll_all_dice(self):
-        for dice in self.children:
-             dice.roll()
-
-class RollButton(Button):
-    pass
-    
-class ScoreOption(BoxLayout):
-    def __init__(self, text, value, **kwargs):
-        super(ScoreOption, self).__init__()
-        self.text = text
-        self.value = value
-
-class ScoreCard(BoxLayout):
-    def __init__(self, **kwargs):
-        super(ScoreCard,self).__init__()
-        score_card_dict = {}
-        for entry in score_types:
-            score_card_dict[entry] = None
-            self.add_widget(ScoreOption(text = entry, value = 0))
-        score_card_dict = DictProperty
-        #idea is that this will be used to store scores later...
-    pass
-    
+   
 
 if __name__ == '__main__':
     YahtzeeApp().run()
--- kivy_yahtzee/yahtzee_definitions.py
+++ kivy_yahtzee/yahtzee_definitions.py
@@ -1,9 +1,39 @@
 import random
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
+from kivy.properties import NumericProperty, DictProperty
+from kivy.uix.widget import Widget
+
+
 
 score_types = ["Aces","Twos","Threes","Fours","Fives","Sixes",
          "Three of a Kind","Four of a Kind","Full House", "Small Straight",
          "Large Straight", "Chance", "Yahtzee", "Yahtzee Bonus"]
 
 
+class DiceLayer(BoxLayout):
+    def roll_all_dice(self):
+        for dice in self.children:
+             dice.roll()
 
+class RollButton(Button):
+    pass
+    
+class ScoreOption(BoxLayout):
+    def __init__(self, text, value, **kwargs):
+        super(ScoreOption, self).__init__()
+        self.text = text
+        self.value = value
 
+class ScoreCard(BoxLayout):
+    def __init__(self, **kwargs):
+        super(ScoreCard,self).__init__()
+        score_card_dict = {}
+        for entry in score_types:
+            score_card_dict[entry] = None
+            self.add_widget(ScoreOption(text = entry, value = 0))
+        score_card_dict = DictProperty
+        #idea is that this will be used to store scores later...
+    pass
+ 
