5a9eb4b0fe5bf5bcefc496a3d73fbacce1c8309a
ksullivan2 <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-02-01 15:42:56 -0500

    action button text binds to game.instructions

--- kivy_yahtzee/__main__.py
+++ kivy_yahtzee/__main__.py
@@ -31,26 +31,26 @@ class YahtzeeGame(BoxLayout):
         if self.gamestate == 1:
             self.ids["dice_layer"].enable_dice()
             self.ids["dice_layer"].roll_all_dice()
-            self.ids["actionbutton"].text = "Roll again."
+            self.instructions = "Roll again."
             
         elif self.gamestate == 2:
             self.ids["dice_layer"].roll_all_dice()
-            self.ids["actionbutton"].text = "Final roll."
+            self.instructions = "Final roll."
             
         elif self.gamestate == 3:
             self.ids["dice_layer"].roll_all_dice()
-            self.ids["actionbutton"].text = "Choose where to put your points."
+            self.instructions = "Choose where to put your points."
             self.ids["actionbutton"].disabled = True
             self.ids["scorecard"].enable_score_options()
             self.ids["dice_layer"].disable_dice()
             self.show_scores_in_scorecard()
         elif self.gamestate == 4:
-           self.ids["actionbutton"].text = "Confirm points?" 
+           self.instructions = "Confirm points?" 
            self.ids["actionbutton"].disabled = False
         elif self.gamestate == 5: 
             self.ids["scorecard"].disable_score_options()
             self.ids["scorecard"].select_score()
-            self.ids["actionbutton"].text = "Roll your next hand."
+            self.instructions = "Roll your next hand."
             
         
 
