46cdcf6f591b7b478a59d3db2488981c1978a9e6
ksullivan2 <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-02-01 00:20:56 -0500

    cleaned up redundancies

--- kivy_yahtzee/__main__.py
+++ kivy_yahtzee/__main__.py
@@ -16,36 +16,31 @@ class YahtzeeGame(BoxLayout):
     #0 is ready for first roll, first turn only
     #1 is ready for 2nd roll
     #2 is ready for final roll
-    #3 is dice set, choose score vall;;o85¿‚Æ(                               ue
+    #3 is dice set, choose score value
     
     def change_game_state(self):
+        self.ids["dice_layer"].roll_all_dice()
         if self.state == 0:
-            self.ids["dice_layer"].roll_all_dice()
             self.ids["actionbutton"].text = "Roll again."
             self.state = 1
         elif self.state == 1:
-            self.ids["dice_layer"].roll_all_dice()
             self.ids["actionbutton"].text = "Final roll."
             self.state = 2
         elif self.state == 2:
-            self.ids["dice_layer"].roll_all_dice()
             self.ids["actionbutton"].text = "Confirm points and roll again."
             self.state = 3
         elif self.state == 3:
-            #choose score value here
-            self.ids["dice_layer"].roll_all_dice()
+            #choose score value here  
             self.ids["actionbutton"].text = "Roll again."
             self.state = 1
         
+        
 
 
-class DiceLayer(BoxLayout):
-    roll_count = NumericProperty(0)
-
+class DiceLayer(BoxLayout):  
     def roll_all_dice(self):
         for dice in self.children:
              dice.roll()
-             self.roll_count += 1
         
 
 class RollButton(Button):
