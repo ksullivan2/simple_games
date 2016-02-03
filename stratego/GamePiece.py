5ee0c5c3443cef22aa67e0820a0cdd9f3230cb16
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-02-03 18:17:40 -0500

    has a label with the number now

--- stratego/GamePiece.py
+++ stratego/GamePiece.py
@@ -1,7 +1,27 @@
 from kivy.uix.boxlayout import BoxLayout
 
+
+
+
 class GamePiece(BoxLayout):
     def __init__(self, number, **kwargs):
-        super(GamePiece,self).__init__()
         self.number = number
-    pass
+        super(GamePiece, self).__init__()
+
+
+
+    def get_name(self):
+        names = {0: "Flag",
+                 1: "Spy",
+                 2: "Scout",
+                 3: "Miner",
+                 4: "Sergeant",
+                 5: "Lieutenant",
+                 6: "Captain",
+                 7: "Major",
+                 8: "Colonel",
+                 9: "General",
+                 10: "Marshal",
+                 11: "Bomb"}
+        return names[self.number]
+
