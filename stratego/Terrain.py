9177ccb15be495f3c92a4cb988a6336f0984d8cf
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-02-04 11:24:17 -0500

    board has water now

--- stratego/Terrain.py
+++ stratego/Terrain.py
@@ -1,8 +1,15 @@
 from kivy.uix.button import Button
 
 class Terrain(Button):
-    def __init__(self, row, col):
+    def __init__(self, row, col, land = True):
         self.row = row
         self.col = col
+        self.id = str(self.row) + "," + str(self.col)
+        self.land = land
         super(Terrain,self).__init__()
 
+    def get_background_image(self):
+        if self.land:
+            return "images/land.png"
+        else:
+            return "images/water.png"
--- stratego/__main__.py
+++ stratego/__main__.py
@@ -4,6 +4,10 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from GamePiece import *
 from Terrain import *
+from kivy.core.window import Window
+
+Window.size = (600, 600)
+
 
 
 class StrategoApp(App):
@@ -20,7 +24,11 @@ class StrategoBoard(GridLayout):
             self.board.append([])
             for j in range(10):
                 self.board[i].append(j)
-                self.add_widget(Terrain(i,j))
+                if i in (4,5) and j in (2,3,6,7):
+                    self.add_widget(Terrain(i,j, land=False))
+                else:
+                    self.add_widget(Terrain(i,j))
+
 
 
 
