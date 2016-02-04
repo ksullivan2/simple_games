3edac68c0a7f27e562d53c31782bd68702e7d8fd
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-02-04 14:06:31 -0500

    put board background into relative layout

---
+++ stratego/StrategoBoard.py
@@ -0,0 +1,20 @@
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.relativelayout import RelativeLayout
+from Terrain import *
+
+
+class StrategoBoard(RelativeLayout):
+    pass
+
+class BoardBackground(GridLayout):
+    def __init__(self, **kwargs):
+        self.grid = []
+        super(BoardBackground, self).__init__()
+        for i in range(10):
+            self.grid.append([])
+            for j in range(10):
+                self.grid[i].append(j)
+                if i in (4,5) and j in (2,3,6,7):
+                    self.add_widget(Terrain(i,j, land=False))
+                else:
+                    self.add_widget(Terrain(i,j))
--- stratego/__main__.py
+++ stratego/__main__.py
@@ -2,9 +2,15 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.core.window import Window
+
+
+
 from GamePiece import *
 from Terrain import *
-from kivy.core.window import Window
+from StrategoBoard import *
+
+
 
 Window.size = (700, 700)
 
@@ -14,23 +20,6 @@ class StrategoApp(App):
     def build(self):
         game = StrategoBoard()
         return game
-
-class StrategoBoard(GridLayout):
-
-    def __init__(self, **kwargs):
-        self.board = []
-        super(StrategoBoard, self).__init__()
-        for i in range(10):
-            self.board.append([])
-            for j in range(10):
-                self.board[i].append(j)
-                if i in (4,5) and j in (2,3,6,7):
-                    self.add_widget(Terrain(i,j, land=False))
-                else:
-                    self.add_widget(Terrain(i,j))
-
-
-
 
 
 
